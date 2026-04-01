0fbe1a47e136c4d83b4f401166d2df65e64a9251
ishajain01091999-ops <ishajain01091999@gmail.com>
2026-04-01 12:07:37 +0530

    Update test_extension.py

--- test_extension.py
+++ test_extension.py
@@ -24,8 +24,12 @@ WEBSITES = [
 # =========================================================
 options = Options()
 options.add_argument("--headless=new")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--disable-gpu")
 options.add_argument("--window-size=1920,1080")
+
+driver = webdriver.Chrome(options=options)
 
 driver = webdriver.Chrome(
     service=Service(ChromeDriverManager().install()),
